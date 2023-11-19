1b6da9f962dbef7c39b5999ff5956ac331909293
brunogsr <brunogsr https://github.com/brunogsr>
2023-11-18 22:16:17 -0300

    req 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,23 @@
-# Req 3
+import csv
+from src.models.ingredient import Ingredient
+from src.models.dish import Dish
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        with open(source_path, 'r') as file:
+            reader = csv.reader(file)
+            next(reader)  # Skip header row
+            for row in reader:
+                name, price, ingredient, amount = row
+                dish_instance = Dish(name, float(price))
+                if dish_instance not in self.dishes:
+                    dish_instance.add_ingredient_dependency(
+                        Ingredient(ingredient), int(amount))
+                    self.dishes.add(dish_instance)
+                else:
+                    for dish in self.dishes:
+                        if dish == dish_instance:
+                            dish.add_ingredient_dependency(
+                                Ingredient(ingredient), int(amount))
