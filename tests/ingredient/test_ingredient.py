57c2e1bf4e1de7f318c99f052e5a9590492698e5
brunogsr <brunogsr https://github.com/brunogsr>
2023-11-18 21:14:14 -0300

    req 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,16 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 
-# Req 1
 def test_ingredient():
-    pass
+    GLUTEN = "GLUTEN"
+    LACTOSE = "LACTOSE"
+
+    gluten_instance = Ingredient(GLUTEN)
+    lactose_instance = Ingredient(LACTOSE)
+
+    assert gluten_instance.name == GLUTEN
+    assert gluten_instance == gluten_instance
+    assert hash(gluten_instance) == hash(GLUTEN)
+    assert repr(gluten_instance) == f"Ingredient('{GLUTEN}')"
+    assert gluten_instance.restrictions == set()
+    assert gluten_instance != lactose_instance
